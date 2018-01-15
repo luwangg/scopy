b2e1f0a4ba58f8c8c705cea791b5a7cda005b451
AlexandraTrifan <Alexandra.Trifan@analog.com>
2018-01-15 10:45:54 +0200

    CI/travis/before_install_darwin.sh: Fix OSX 10.11 build.

--- CI/travis/before_install_darwin.sh
+++ CI/travis/before_install_darwin.sh
@@ -7,15 +7,15 @@ echo 'export PATH="$(brew --prefix qt)/bin:$PATH"' >> ~/.bash_profile
 brew link --force qt
 
 brew unlink bison
-brew install cmake fftw bison autoconf automake libtool libzip glib glibmm doxygen python3 wget boost gnu-sed libmatio dylibbundler
+brew install cmake bison autoconf automake libtool libzip glib glibmm doxygen python3 wget boost gnu-sed libmatio dylibbundler
+brew search fftw@
+brew install fftw@3.3.7
 brew install llvm
 brew link --overwrite --force gcc
 brew link --overwrite --force bison
 brew link --overwrite --force gettext
 brew upgrade cmake
 brew upgrade bison
-brew upgrade fftw
-
 
 # Required for Python
 export PYTHONPATH=$(brew --prefix)/lib/python2.7/site-packages
